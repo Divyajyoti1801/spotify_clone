6c351554dbb7a66a6314c671f5ef00defa97c81b
Divyajyoti <31211823+Divyajyoti1801@users.noreply.github.com>
2023-06-03 02:03:07 +0530

    Project Setup sidebar basic design completed

---
+++ app/layout.tsx
@@ -0,0 +1,25 @@
+import { Figtree } from "next/font/google";
+import Sidebar from "./components/Sidebar";
+import "./globals.css";
+
+const font = Figtree({ subsets: ["latin"] });
+
+export const metadata = {
+  title: "Spotify: Clone App",
+  description:
+    "Spotify is a digital music, podcast, and video service that gives you access to millions of songs and other content from creators all over the world. Basic functions such as playing music are totally free, but you can also choose to upgrade to Spotify Premium.",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body className={font.className}>
+        <Sidebar>{children}</Sidebar>
+      </body>
+    </html>
+  );
+}
