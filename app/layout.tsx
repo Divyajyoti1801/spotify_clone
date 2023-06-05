716977d6a9b5597358771649a6c6f18eea788f36
Divyajyoti <31211823+Divyajyoti1801@users.noreply.github.com>
2023-06-05 22:48:37 +0530

    Major components completed

---
+++ app/layout.tsx
@@ -0,0 +1,25 @@
+import Sidebar from "@/components/Sidebar";
+import { Figtree } from "next/font/google";
+import "./globals.css";
+
+const font = Figtree({ subsets: ["latin"] });
+
+export const metadata = {
+  title: "Spotify: Clone",
+  description:
+    "Spotify is a digital music service that gives you access to millions of songs.",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body className={font.className}>
+        <Sidebar>{children}</Sidebar>
+      </body>
+    </html>
+  );
+}
---
+++ components/SidebarItem.tsx
@@ -0,0 +1,36 @@
+import Link from "next/link";
+import { useRouter } from "next/navigation";
+import { IconType } from "react-icons";
+import { twMerge } from "tailwind-merge";
+
+interface SidebarItemProps {
+  icon: IconType;
+  label: string;
+  active?: boolean;
+  href: string;
+  className?: string;
+}
+
+const SidebarItem: React.FC<SidebarItemProps> = ({
+  icon: Icon,
+  label,
+  active,
+  href,
+  className,
+}) => {
+  return (
+    <Link
+      href={href}
+      className={twMerge(
+        `flex flex-row h-auto items-center w-full gap-x-4 text-md  font-medium cursor-pointer hover:text-white transition text-neutral-400 py-1`,
+        active && "text-white",
+        className
+      )}
+    >
+      <Icon size={26} />
+      <p className="truncate w-full">{label}</p>
+    </Link>
+  );
+};
+
+export default SidebarItem;
